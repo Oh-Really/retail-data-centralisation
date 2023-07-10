e23e994008a9e8cf3968f964a4671a315e3fbbd2
Oh-Really <moreilly406@gmail.com>
2023-07-10 20:01:30 +0100

    Updated main file for sales date df

--- main.py
+++ main.py
@@ -27,6 +27,10 @@ def upload_card_details():
      table = DataExtractor()
      df = table.retrieve_pdf_data()
      df = dc.clean_card_data(df)
+     cred = database.read_db_creds("db_creds_local.yaml")
+     engine = database.init_db_engine(cred)
+     engine.connect()
+     database.upload_to_db(df, 'dim_card_details', engine)
      return df
 
 #store info
@@ -54,11 +58,41 @@ def upload_product_info():
       database.upload_to_db(df, 'dim_products', engine)
       return df
 
+def order_data():
+     dc = DataCleaning()
+     database = DatabaseConnector()
+     de = DataExtractor()
+     cred = database.read_db_creds("db_creds.yaml")
+     engine = database.init_db_engine(cred)
+     df = de.read_rds_table(engine, 'orders_table')
+     df = dc.clean_orders_data(df)
+     cred = database.read_db_creds("db_creds_local.yaml")
+     engine = database.init_db_engine(cred)
+     engine.connect()
+     database.upload_to_db(df, 'orders_table', engine)
+     return df
+
+def sale_dates():
+     dc = DataCleaning()
+     database = DatabaseConnector()
+     de = DataExtractor()
+     df = de.retrieve_json_products()
+     df = dc.clean_sales_data(df)
+     cred = database.read_db_creds("db_creds_local.yaml")
+     engine = database.init_db_engine(cred)
+     engine.connect()
+     database.upload_to_db(df, 'dim_date_times', engine)
+     return df
+
+      
+
 if __name__ == '__main__':
      user_df = upload_dim_users()
      card_df = upload_card_details()
      stores_df = upload_store_info()
      s3 = upload_product_info()
+     order_df = order_data()
+     sales_df = sale_dates()
 
      # dc = DataCleaning()
      # database = DatabaseConnector()
